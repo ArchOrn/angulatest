98c9834fbecefb9eef776e5eae9c13053150adab
Raphaël 'ArchOrn' KIFFER <kiffer.raphael@gmail.com>
2015-04-13 02:15:35 +0200

    Add some interaction.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,12 +4,24 @@ app.controller('HomeCtrl', function ($scope, $http, $localStorage) {
   // Default fact identifier value
   $scope.$storage = $localStorage.$default({ factId: 0 });
 
+  $scope.units = {
+    percentage: '%',
+    meters: 'm'
+  };
+
+  $scope.leagues = {};
+  $scope.leagueIndex = 0;
+
+  $scope.chooseLeague = function (league) {
+    if ($scope.leagues[0] != league && $scope.leagues[1] != league) {
+      $scope.leagues[$scope.leagueIndex] = league;
+      $scope.leagueIndex = ($scope.leagueIndex == 0 ? 1 : 0);
+      console.log($scope.leagues);
+    }
+  };
+
   $http.get('/static/facts.json').then(function (result) {
     $scope.facts = result.data;
-    $scope.units = {
-      percentage: '%',
-      meters: 'm'
-    };
 
     $scope.roll = function () {
       var id = 0;
@@ -18,7 +30,7 @@ app.controller('HomeCtrl', function ($scope, $http, $localStorage) {
       } while (id == $scope.$storage.factId);
       $scope.$storage.factId = id;
       $scope.randomFact = $scope.facts[id];
-    }
+    };
 
     $scope.roll();
   });
