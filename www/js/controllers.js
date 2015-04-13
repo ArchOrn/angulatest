8472eeadcf3da5f06c6b8c6c86c660c8cbce0ff8
Raphaël 'ArchOrn' KIFFER <kiffer.raphael@gmail.com>
2015-04-14 01:36:54 +0200

    Update design.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,22 @@
-var app = angular.module('UrfMadnessApp', ['ngStorage']);
+var app = angular.module('UrfMadnessAppControllers', ['ngStorage']);
 
 app.controller('HomeCtrl', function ($scope, $http, $localStorage) {
   // Default fact identifier value
   $scope.$storage = $localStorage.$default({ factId: 0 });
 
-  $scope.units = {
-    percentage: '%',
-    meters: 'm'
+  $scope.leagues = {
+    bronze: false,
+    silver: false,
+    gold: false,
+    platinum: false,
+    diamond: false,
+    master: false,
+    challenger: false
   };
 
-  $scope.leagues = {};
-  $scope.leagueIndex = 0;
-
   $scope.chooseLeague = function (league) {
-    if ($scope.leagues[0] != league && $scope.leagues[1] != league) {
-      $scope.leagues[$scope.leagueIndex] = league;
-      $scope.leagueIndex = ($scope.leagueIndex == 0 ? 1 : 0);
-      console.log($scope.leagues);
+    if (typeof($scope.leagues[league]) != 'undefined') {
+      $scope.leagues[league] = !$scope.leagues[league];
     }
   };
 
